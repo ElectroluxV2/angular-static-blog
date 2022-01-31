da6edb9249e18219c2cb38482a413a936f9b5724
electroluxv2 <electroluxv2@gmail.com>
2022-01-31 02:18:59 +0100

    Fixed pre-rendering

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -3,21 +3,23 @@ import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 import { Blog } from '../interfaces/blog';
 import { Post } from '../interfaces/post.interface';
+import { isPlatformServer } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
   #blog?: Blog;
+  readonly #b;
 
-  constructor(private http: HttpClient, @Inject(PLATFORM_ID) platformId: string) {
-
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: string) {
+    this.#b = isPlatformServer(this.platformId) ? 'http://localhost:8080' : '';
   }
 
   public async getBlog(): Promise<Blog> {
     if (!!this.#blog) return this.#blog;
 
-    const metadata = await firstValueFrom(this.http.get<{posts: Post[]}>('/assets/blog/blog.json'));
+    const metadata = await firstValueFrom(this.http.get<{posts: Post[]}>(`${this.#b}/assets/blog/blog.json`));
     this.#blog = new Blog(metadata.posts.map(post => [post.document.slug, post]));
 
     return this.#blog;
@@ -29,7 +31,7 @@ export class BlogService {
 
     const post = this.#blog.get(slug);
     if (!post.document.html) {
-      post.document.html = await firstValueFrom(this.http.get(`/assets/blog/${slug}.html`, {responseType: 'text'}));
+      post.document.html = await firstValueFrom(this.http.get(`${this.#b}/assets/blog/${slug}.html`, {responseType: 'text'}));
     }
 
     return post;
